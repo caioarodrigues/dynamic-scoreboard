9a996c6830d08c91305169d6451bf56bed8b8900
Caioã <caioarodrigues@alu.ufc.br>
2024-04-10 17:01:18 -0300

    resolvido o bug de loop infinito

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import "./index.css"
-import { Card } from './Components/Card/Index.module'
 import { CardsList } from "./Components/CardsList/Index.module"
-import { useCardsContext } from "./Context/CardsContext"
-import { useCallback } from "react"
 
 function App() {
-  const { getCards, createCard } = useCardsContext()
-
-  const _createCard = useCallback((name) => {
-    createCard({ name })
-  }, [getCards])
-
-  _createCard('João')
-  console.log(getCards())
   return (
     <div className="bg-slate-400 h-screen">
       <CardsList />
-      <Card name='João' />
-      <Card name='Maria' />  
-      <Card name='José' />  
     </div>
   )
 }
--- src/Components/CardsList/Index.module.tsx
+++ src/Components/CardsList/Index.module.tsx
@@ -3,10 +3,14 @@ import { useCard } from "../../hooks/useCard";
 import { Button } from "../Button/Index.module";
 import { useState } from "react";
 import { useRef } from "react";
+import { useCardsContext } from "../../Context/CardsContext";
+import { Card } from "../Card/Index.module";
 
 export const CardsList = () => {
-  const { createCard } = useCard();
+  //const { createCard } = useCard();
   const [inputText, setInputText] = useState("");
+  const { createCard, getCards } = useCardsContext();
+  const cards = getCards();
   let inputRef = useRef<HTMLInputElement>(null);
 
   const clearInputText = () => {
@@ -25,10 +29,14 @@ export const CardsList = () => {
       <Button
         text="new card"
         handler={() => {
-          createCard({ name: inputText });
+          createCard(inputText);
           clearInputText();
         }}
       />
+
+      {cards.map(({ name }, index) => (
+        <Card name={name} key={index} />
+      ))}
     </div>
   );
 };
